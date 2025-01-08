d45c1b05fff61fb91ef5d5d3bfa2b8b57a965a9b
Saharat <saharat.on@ku.th>
2025-01-08 23:55:36 +0700

    Add files via upload

---
+++ hangman/wordlist.py
@@ -0,0 +1,15 @@
+#word for hangman game
+
+word = [
+    "aardvark", "albatross", "alligator", "alpaca", "ant", "anteater", "antelope", "ape", "armadillo", "donkey",
+    "baboon", "badger", "barracuda", "bat", "bear", "beaver", "bee", "bison", "boar", "buffalo",
+    "butterfly", "camel", "capybara", "caribou", "cassowary", "cat", "caterpillar", "cattle", "chamois", "cheetah",
+    "chicken", "chimpanzee", "chinchilla", "chough", "clam", "cobra", "cockroach", "cod", "cormorant", "coyote",
+    "crab", "crane", "crocodile", "crow", "curlew", "deer", "dinosaur", "dog", "dogfish", "dolphin",
+    "dotterel", "dove", "dragonfly", "duck", "dugong", "dunlin", "eagle", "echidna", "eel", "eland",
+    "elephant", "elk", "emu", "falcon", "ferret", "finch", "fish", "flamingo", "fly", "fox",
+    "frog", "gaur", "gazelle", "gerbil", "giraffe", "gnat", "gnu", "goat", "goldfinch", "goldfish",
+    "goose", "gorilla", "goshawk", "grasshopper", "grouse", "guanaco", "guinea fowl", "guinea pig", "gull", "hamster",
+    "hare", "hawk", "hedgehog", "heron", "herring", "hippopotamus", "hornet", "horse", "human", "hummingbird"
+]
+
